76608f4f61fba9aa65d4abc6e0effdfbfdacd78f
unknown <mowlesar.kathir@gmail.comcd bootcampOmit --git config mowlesar.kathir@gmail.com>
2023-06-27 16:00:39 -0400

    Added all of the questions and answers

--- Assets/script.js
+++ Assets/script.js
@@ -5,7 +5,118 @@ var questionText= document.getElementById("question")
 var optionList = document.getElementById("options-list")
 var feedbackText = document.getElementById("feedback-text")
 var timerText = document.getElementById("timer-text")
-var timer = document.getElementById("timer")
 var initalsContainer = document.getElementById("initals-container")
 var highscoresLinkContainer = document.getElementById("highscores-link-container")
 
+// set up empty variables for score and timer
+
+var timer;
+var timeLeft;
+var currentQuestionsIndex = 0;
+var score = 0;
+var quizStart = false;
+
+// set up questions
+
+var questions = [
+  {
+    question: "What is JavaScript?",
+    options: [
+      "A programming language",
+      "A markup language",
+      "A styling language",
+      "A scripting language"
+    ],
+    correctAnswer: 0
+  },
+  {
+    question: "What does DOM stand for?",
+    options: [
+      "Document Oriented Model",
+      "Data Object Model",
+      "Document Object Model",
+      "Data Oriented Model"
+    ],
+    correctAnswer: 2
+  },
+  {
+    question: "What is the correct HTML tag for insterting a line break?",
+    options: [
+      "<br>",
+      "<break>",
+      "<lb>",
+      "<line>"
+    ],
+    correctAnswer: 0
+  },
+  {
+    question: "How would you select an element with the class hero in CSS?",
+    options: [
+      "A programming language",
+      "A markup language",
+      "A styling language",
+      "A scripting language"
+    ],
+    correctAnswer: 0
+  },
+  {
+    question: "Which of the following is NOT a JavaScript data type?",
+    options: [
+      "*example",
+      "example",
+      "#example",
+      ".example"
+    ],
+    correctAnswer: 3
+  },
+  {
+    question: "Which of the following is NOT a JavaScript data type?",
+    options: [
+      "String",
+      "Number",
+      "Float",
+      "Boolean"
+    ],
+    correctAnswer: 2
+  },
+  {
+    question: " What does the <img> tag in HTML stand for?",
+    options: [
+      "Import",
+      "Image",
+      "Inline",
+      "Insert"
+    ],
+    correctAnswer: 1
+  },
+  {
+    question: "How can you add a background color to an HTML element using CSS?",
+    options: [
+      "background-color: red;",
+      "color: red;",
+      "text-color: red;",
+      "background: red;"
+    ],
+    correctAnswer: 0
+  },
+  {
+    question: "Which HTML tag is used to define an unordered list?",
+    options: [
+      "<ol>",
+      "<li>",
+      "<ul>",
+      "<list>"
+    ],
+    correctAnswer: 3
+  },
+  {
+    question: "How do you write a comment in JavaScript?",
+    options: [
+      "<!-- This is a comment -->",
+      "// This is a comment",
+      "/* This is a comment */",
+      "# This is a comment"
+    ],
+    correctAnswer: 1
+  },
+]
