dea09e118991c09575f6aab44d7bee3cd4f245c0
unknown <mowlesar.kathir@gmail.comcd bootcampOmit --git config mowlesar.kathir@gmail.com>
2023-06-28 10:58:53 -0400

    Added several functions

--- Assets/script.js
+++ Assets/script.js
@@ -123,9 +123,150 @@ var questions = [
 
 startButton.addEventListener("click", startQuiz);
 
-function startQuiz() 
+function startQuiz() {
+  quizStarted = true;
+  introContainer.style.display = "none";
+  initalsContainer.style.display = "none";
+  highscoresLinkContainer.style.display = "none";
+  quizContainer.style.display = "block";
 
-function showQuestion()
+  timeLeft = 60;
+
+  timer = setInterval(() => {
+    timeLeft--;
+    timerText.textContent = timeLeft;
+
+    if (timeLeft <= 0) {
+      endQuiz ()
+    }
+}, 1000);
+
+showQuestion();
+}
+
+function showQuestion() {
+  var randomIndex = Math.floor(Math.random() * questions.length);
+  var question = questions[randomIndex];
+  questions.splice(randomIndex, 1);
+  currentQuestionsIndex++;
+
+  questionText.textContent = question.question;
+  choicesList.innerHTML = "";
+
+  for (var i = 0; i < question.choices.length; i++) {
+    var choiceElement = document.createElement("li");
+    choiceElement.classList.add("option");
+    var choiceButton = document.createElement("button");
+    choiceButton.textContent = question.choices[i];
+    choiceElement.appendChild(choiceButton);
+    choicesList.appendChild(choiceElement);
+  }
+
+  if (questions.length === 0) {
+    questions = [
+      {
+        question: "What is JavaScript?",
+        options: [
+          "A programming language",
+          "A markup language",
+          "A styling language",
+          "A scripting language"
+        ],
+        correctAnswer: 0
+      },
+      {
+        question: "What does DOM stand for?",
+        options: [
+          "Document Oriented Model",
+          "Data Object Model",
+          "Document Object Model",
+          "Data Oriented Model"
+        ],
+        correctAnswer: 2
+      },
+      {
+        question: "What is the correct HTML tag for insterting a line break?",
+        options: [
+          "<br>",
+          "<break>",
+          "<lb>",
+          "<line>"
+        ],
+        correctAnswer: 0
+      },
+      {
+        question: "How would you select an element with the class hero in CSS?",
+        options: [
+          "A programming language",
+          "A markup language",
+          "A styling language",
+          "A scripting language"
+        ],
+        correctAnswer: 0
+      },
+      {
+        question: "Which of the following is NOT a JavaScript data type?",
+        options: [
+          "*example",
+          "example",
+          "#example",
+          ".example"
+        ],
+        correctAnswer: 3
+      },
+      {
+        question: "Which of the following is NOT a JavaScript data type?",
+        options: [
+          "String",
+          "Number",
+          "Float",
+          "Boolean"
+        ],
+        correctAnswer: 2
+      },
+      {
+        question: " What does the <img> tag in HTML stand for?",
+        options: [
+          "Import",
+          "Image",
+          "Inline",
+          "Insert"
+        ],
+        correctAnswer: 1
+      },
+      {
+        question: "How can you add a background color to an HTML element using CSS?",
+        options: [
+          "background-color: red;",
+          "color: red;",
+          "text-color: red;",
+          "background: red;"
+        ],
+        correctAnswer: 0
+      },
+      {
+        question: "Which HTML tag is used to define an unordered list?",
+        options: [
+          "<ol>",
+          "<li>",
+          "<ul>",
+          "<list>"
+        ],
+        correctAnswer: 3
+      },
+      {
+        question: "How do you write a comment in JavaScript?",
+        options: [
+          "<!-- This is a comment -->",
+          "// This is a comment",
+          "/* This is a comment */",
+          "# This is a comment"
+        ],
+        correctAnswer: 1
+      },
+    ];
+  }
+}
 
 function endQuiz ()
 
